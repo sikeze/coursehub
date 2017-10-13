3ecfe4adc7c31129d4e16e43702ab577331e6211
Mark Michaelsen <mmichaelsen678@gmail.com>
2017-10-13 13:25:29 -0300

    Initial commit

---
+++ lang/en/local_coursehub.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Strings for component "coursehub", language "en"
+ *
+ * @package	coursehub
+ */
+
+defined("MOODLE_INTERNAL") || die();
+
+//$string[""] = "";
+
+$string["page_title"] = "Course Hub";
+$string["page_heading"] = "Course Hub";
---
+++ lang/es/local_coursehub.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Strings for component "coursehub", language "es"
+ *
+ * @package	coursehub
+ */
+
+defined("MOODLE_INTERNAL") || die();
+
+//$string[""] = "";
+
+$string["page_title"] = "Centro de Cursos";
+$string["page_heading"] = "Centro de Cursos";
